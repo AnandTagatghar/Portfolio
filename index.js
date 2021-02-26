5fe68983d4cbe474373e1316f3daf65fe66ec508
AnandTagatgar <79243140+AnandTagatgar@users.noreply.github.com>
2021-02-26 23:26:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var a = 'hello';
 document.getElementById("demo").innerHTML = a;
+time();
+function time()
+{
+  var a = new Date();
+  var b = a.getHours()+':'+a.getMinutes()+':'+a.getSeconds();
+  document.getElementById('time').innerHTML = b;
+  setTimeout(time,1000);
+}
